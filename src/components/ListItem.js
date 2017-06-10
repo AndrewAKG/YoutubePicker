43f22b9fb055ed329f63a3efb7675483895224e4
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-06-11 01:51:21 +0200

    Youtube-Picker with Bonus(msh awy y3ny bs el video sh8al)

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
-import { Linking, Image, TouchableWithoutFeedback, Alert } from 'react-native';
+import { Linking, Image, TouchableWithoutFeedback, Alert, WebView } from 'react-native';
 import {
     Container, Content, Card, CardItem,
     Text, Button, Thumbnail,
@@ -62,6 +62,24 @@ class ListItem extends Component {
         const { title, description, thumbnails, channelTitle, publishedAt } = this.props.video.snippet;
         const { url } = thumbnails.high;
         const link = "https://www.youtube.com/watch?v=" + videoId;
+        const playLink = '<iframe id="player" type="text/html" width="stretch" height="390" src="http://www.youtube.com/embed/' + videoId + '?enablejsapi=1&origin=http://example.com"></iframe>'
+            + '<script> var tag = document.createElement(\'script\');'
+            + 'tag.src = "https://www.youtube.com/iframe_api";'
+            + 'var firstScriptTag = document.getElementsByTagName(\'script\')[0];'
+            + 'firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);'
+            + 'var player;'
+            + 'function onYouTubeIframeAPIReady() {'
+            + 'player = new YT.Player(\'player\', { events: { \'onReady\': onPlayerReady,\'onStateChange\': onPlayerStateChange   } });}'
+            + 'function onPlayerReady(event) {'
+            + 'event.target.playVideo(); }'
+            + 'var done = false;'
+            + 'function onPlayerStateChange(event) {'
+            + 'if (event.data == YT.PlayerState.PLAYING && !done) {'
+            + 'setTimeout(stopVideo, 6000);'
+            + 'done = true; } }'
+            + 'function pauseVideo(){ player.pauseVideo();}'
+            + 'function stopVideo() {    player.stopVideo(); } </script>';
+
 
         var videoDate = new Date(publishedAt);
         var videoYear = videoDate.getFullYear();
@@ -76,14 +94,12 @@ class ListItem extends Component {
                                 <Text style={styles.TitleStyle}>{title}</Text>
                             </Body>
                         </CardItem>
-                        <TouchableWithoutFeedback
-                            onPress={() => Linking.openURL(link)}>
-                            <CardItem>
-                                <Image
-                                    style={styles.ImageStyle}
-                                    source={{ uri: url }} />
-                            </CardItem>
-                        </TouchableWithoutFeedback>
+                        <CardItem>
+                            <WebView
+                                source={{ html: playLink }}
+                                style={styles.ImageStyle}
+                            />
+                        </CardItem>
                         <CardItem>
                             <Body>
                                 <Text style={styles.TextStyle}>
@@ -138,3 +154,15 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps,
     { AddingToFavourites, InitiatingState, FavouriteDelete })
     (ListItem);
+
+/*
+//Clickable Image
+<TouchableWithoutFeedback
+    onPress={() => Linking.openURL(link)}>
+    <CardItem>
+        <Image
+            style={styles.ImageStyle}
+            source={{ uri: url }} />
+    </CardItem>
+</TouchableWithoutFeedback>
+*/
